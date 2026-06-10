54553818acafaf39b2ba3af416f9d8ccb6ddd49f
lahiru-chathumina <chathuminabandaralahiru@gmail.com>
2026-06-11 00:59:07 +0530

    Updated dbs

--- src/main/java/org/heartmatch/repository/MatchRepository.java
+++ src/main/java/org/heartmatch/repository/MatchRepository.java
@@ -2,19 +2,15 @@ package org.heartmatch.repository;
 
 import org.heartmatch.entity.Match;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
-import java.util.Optional;
+import java.util.UUID;
 
 @Repository
-public interface MatchRepository extends JpaRepository<Match, Long> {
-    Optional<Match> findByInitiatorIdAndTargetId(Long initiatorId, Long targetId);
-    List<Match> findByInitiatorId(Long initiatorId);
-    List<Match> findByTargetId(Long targetId);
+public interface MatchRepository extends JpaRepository<Match, UUID> {
 
-    @Query("SELECT m FROM Match m WHERE (m.initiator.id = :userId OR m.target.id = :userId) AND m.isMutual = true")
-    List<Match> findMutualMatches(@Param("userId") Long userId);
+    List<Match> findBySender_Id(UUID senderId);
+
+    List<Match> findByReceiver_Id(UUID receiverId);
 }
--- src/main/java/org/heartmatch/repository/ProfileRepository.java
+++ src/main/java/org/heartmatch/repository/ProfileRepository.java
@@ -1,13 +1,13 @@
 package org.heartmatch.repository;
 
-
 import org.heartmatch.entity.Profile;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
+import java.util.UUID;
 
-@Repository
 public interface ProfileRepository extends JpaRepository<Profile, Long> {
-    Optional<Profile> findByUserId(Long userId);
+
+    Optional<Profile> findByUserId(UUID userId);
+
 }
